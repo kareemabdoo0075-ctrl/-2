5ee45274fb98d1c0a7c0507ec9c3a69e8ae37cc6
kareemabdoo0075-ctrl <kareemabdoo0075@gmail.com>
2026-09-08 18:01:58 +0300

    Update games.js

--- games.js
+++ games.js
@@ -1,1 +1,44 @@
+const games = [
+    // ألعاب القتال والأكشن
+    { 
+        title: "قتال العصي الملحمي", 
+        category: "action", 
+        img: "https://img.gamepix.com/games/stickman-fighter-epic-battle/icon/stickman-fighter-epic-battle.png", 
+        url: "https://play.gamepix.com/stickman-fighter-epic-battle/embed" 
+    },
+    { 
+        title: "بطل القتال Brawl Hero", 
+        category: "action", 
+        img: "https://img.gamepix.com/games/brawlhero/icon/brawlhero.png", 
+        url: "https://play.gamepix.com/brawlhero/embed" 
+    },
+    { 
+        title: "قتال النينجا Ninja Clash", 
+        category: "action", 
+        img: "https://img.gamepix.com/games/ninja-clash-heroes/icon/ninja-clash-heroes.png", 
+        url: "https://play.gamepix.com/ninja-clash-heroes/embed" 
+    },
+
+    // ألعاب السيارات والسرعة
+    { 
+        title: "سباق الدراجات Moto X3M", 
+        category: "racing", 
+        img: "https://img.gamepix.com/games/moto-x3m/icon/moto-x3m.png", 
+        url: "https://play.gamepix.com/moto-x3m/embed" 
+    },
+    { 
+        title: "سباق السرعة Nitro", 
+        category: "racing", 
+        img: "https://img.gamepix.com/games/sprint-club-nitro/icon/sprint-club-nitro.png", 
+        url: "https://play.gamepix.com/sprint-club-nitro/embed" 
+    },
+
+    // ألعاب الذكاء والألغاز
+    { 
+        title: "قص الحبل Cut The Rope", 
+        category: "puzzle", 
+        img: "https://img.gamepix.com/games/cut-the-rope/icon/cut-the-rope.png", 
+        url: "https://play.gamepix.com/cut-the-rope/embed" 
+    }
+];
 
